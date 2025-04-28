ea16b22066e4e22c1c03f32438abf660c32a2356
Mensun <mensun@bu.edu>
2025-04-28 00:18:57 -0400

    finished writing programmatic file upload and vector store using OpenAI API

---
+++ README.md
@@ -0,0 +1,2 @@
+## Chunking Strategy 
+When creating the Vector Store, I relied on OpenAI’s default chunking behavior. OpenAI automatically splits each uploaded Markdown file into smaller chunks optimized for semantic search, typically about 800 tokens per chunk and an overlap of 400 tokens (from OpenAI's official documentation). Embeddings were generated using the text-embedding-3-large model (256 dimensions), and the Assistant retrieves up to 20 relevant chunks per query.
---
+++ scripts/upload_to_openai.py
@@ -0,0 +1,51 @@
+# scripts/upload_to_openai.py
+
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# init OpenAI client for easier API access
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+ARTICLES_DIR = "../articles"
+
+def upload_file(filepath):
+    """
+    Upload a single file content to OpenAI (API upload)
+    """
+    with open(filepath, "rb") as f:
+        file = client.files.create(
+            file=f,
+            purpose="assistants"
+        )
+    return file.id  
+
+def create_vector_store(file_ids):
+    """
+    Create a vector store with uploaded file IDs
+    """
+    vector_store = client.vector_stores.create(
+        name="Optisigns Vector Store",
+        file_ids=file_ids
+    )
+    return vector_store.id
+
+def main():
+    file_ids = []
+    # upload the files
+    for filename in os.listdir(ARTICLES_DIR):
+        filepath = os.path.join(ARTICLES_DIR, filename)
+        file_id = upload_file(filepath)
+        print(f"Uploaded {filename} - File ID: {file_id}")
+        file_ids.append(file_id)
+    # create vector store based on the file ids uploaded
+    vector_store_id = create_vector_store(file_ids)
+    print(f"Vector Store ID: {vector_store_id}")
+    print(f"Uploaded {len(file_ids)} files into Vector Store.")
+
+if __name__ == "__main__":
+    main()
